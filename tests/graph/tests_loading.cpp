abdcc061404290e3e29bf2b72d2ce1cdbc12effc
mcb <mgrbo@web.de>
2025-05-30 14:01:27 +0200

    graph, load_rdf_file

--- tests/graph/tests_loading.cpp
+++ tests/graph/tests_loading.cpp
@@ -5,15 +5,20 @@
 
 using namespace rdf4cpp;
 
+TEST_CASE("load Graph") {
+    Graph set{};
+    std::fstream file{"test_swdf/swdf.nt"};
+    set.load_rdf_data(file, parser::ParsingFlags::none(), nullptr, [](parser::ParsingError e) {
+        FAIL(std::format("{}", e));
+    });
+    CHECK(set.size() == 304592);
+}
+
 TEST_CASE("load dataset") {
     Dataset set{};
-    for (const auto& v : rdf4cpp::parser::RDFFileParser{"test_swdf/swdf.nt"}) {
-        if (v.has_value()) {
-            set.add(v.value());
-        }
-        else {
-            FAIL(v.error());
-        }
-    }
-    CHECK(set.size() == 304592);
+    std::fstream file{"tests_loading_simple.trig"};
+    set.load_rdf_data(file, parser::ParsingFlags::none(), nullptr, [](parser::ParsingError e) {
+        FAIL(std::format("{}", e));
+    });
+    CHECK(set.size() == 4);
 }
