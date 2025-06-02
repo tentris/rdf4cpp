582903cc334b27defa4ec2007def24610183b52b
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2025-06-02 14:40:40 +0200

    Add dataset load integration test (#373)

---
+++ tests/graph/tests_loading.cpp
@@ -0,0 +1,30 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <rdf4cpp.hpp>
+
+using namespace rdf4cpp;
+
+TEST_CASE("load Graph") {
+    Graph set{};
+    std::fstream file{"test_swdf/swdf.nt"};
+    set.load_rdf_data(file, parser::ParsingFlags::none(), nullptr, [](parser::ParsingError e) {
+        FAIL(std::format("{}", e));
+    });
+    CHECK(set.size() == 304592);
+}
+
+TEST_CASE("load dataset") {
+    Dataset set{};
+    std::fstream file{"tests_loading_simple.trig"};
+    set.load_rdf_data(file, parser::ParsingFlags::none(), nullptr, [](parser::ParsingError e) {
+        FAIL(std::format("{}", e));
+    });
+    CHECK(set.size() == 4);
+    int i = 0;
+    for ([[maybe_unused]] const auto& _ : set) {
+        ++i;
+    }
+    CHECK(i == 4);
+    CHECK(set.graph(IRI::make("http://data.semanticweb.org/workshop/admire/2012")).size() == 4);
+}
