b49f6f26f65a66cc35943a1ef4f1df2600b04679
mcb <mgrbo@web.de>
2026-04-28 14:43:59 +0200

    syntax tests

--- tests/util/tests_Regex.cpp
+++ tests/util/tests_Regex.cpp
@@ -6,6 +6,179 @@
 using namespace rdf4cpp::regex;
 
 TEST_SUITE("regex") {
+    TEST_CASE("basic syntax") {
+        CHECK(Regex{"abc"}.regex_match("abc"));
+        CHECK(!Regex{"abc"}.regex_match("_abc"));
+        CHECK(!Regex{"abc"}.regex_match("abc_"));
+
+        CHECK(Regex{"a|b"}.regex_match("a"));
+        CHECK(Regex{"a|b"}.regex_match("b"));
+        CHECK(!Regex{"a|b"}.regex_match("c"));
+
+        CHECK(Regex{"a?"}.regex_match("a"));
+        CHECK(Regex{"a?"}.regex_match(""));
+        CHECK(!Regex{"a?"}.regex_match("aa"));
+
+        CHECK(Regex{"a*"}.regex_match("a"));
+        CHECK(Regex{"a*"}.regex_match("aaaa"));
+        CHECK(Regex{"a*"}.regex_match(""));
+        CHECK(!Regex{"a*"}.regex_match("b"));
+
+        CHECK(!Regex{"a+"}.regex_match(""));
+        CHECK(Regex{"a+"}.regex_match("a"));
+        CHECK(Regex{"a+"}.regex_match("aaaaa"));
+        CHECK(!Regex{"a+"}.regex_match("b"));
+
+        CHECK(!Regex{"a{2,5}"}.regex_match("a"));
+        CHECK(Regex{"a{2,5}"}.regex_match("aa"));
+        CHECK(Regex{"a{2,5}"}.regex_match("aaaaa"));
+        CHECK(!Regex{"a{2,5}"}.regex_match("aaaaaa"));
+        CHECK(!Regex{"a{2,5}"}.regex_match("bbb"));
+
+        CHECK(Regex{"a{2}"}.regex_match("aa"));
+        CHECK(!Regex{"a{2}"}.regex_match("aaa"));
+        CHECK(!Regex{"a{2}"}.regex_match("aaaa"));
+        CHECK(!Regex{"a{2}"}.regex_match("b"));
+
+        CHECK(Regex{"a{2,}"}.regex_match("aa"));
+        CHECK(Regex{"a{2,}"}.regex_match("aaa"));
+        CHECK(!Regex{"a{2,}"}.regex_match("a"));
+        CHECK(!Regex{"a{2,}"}.regex_match("b"));
+
+        CHECK(Regex{"a{0,2}"}.regex_match("aa"));
+        CHECK(!Regex{"a{0,2}"}.regex_match("aaa"));
+        CHECK(Regex{"a{0,2}"}.regex_match("a"));
+        CHECK(!Regex{"a{0,2}"}.regex_match("b"));
+
+        CHECK(Regex{"a{0,0}"}.regex_match(""));
+        CHECK(!Regex{"a{0,0}"}.regex_match("a"));
+
+        CHECK(Regex{"\\.\\\\\\?\\*\\+\\{\\}\\(\\)\\[\\]\\|\\-\\^\\n\\r\\t"}.regex_match(".\\?*+{}()[]|-^\n\r\t"));
+        CHECK(!Regex{"\\."}.regex_match("a"));
+        CHECK(!Regex{"\\\\"}.regex_match("a"));
+        CHECK(!Regex{"\\?"}.regex_match("a"));
+        CHECK(!Regex{"\\*"}.regex_match("a"));
+        CHECK(!Regex{"\\+"}.regex_match("a"));
+        CHECK(!Regex{"\\{"}.regex_match("a"));
+        CHECK(!Regex{"\\}"}.regex_match("a"));
+        CHECK(!Regex{"\\("}.regex_match("a"));
+        CHECK(!Regex{"\\["}.regex_match("a"));
+        CHECK(!Regex{"\\]"}.regex_match("a"));
+        CHECK(!Regex{"\\|"}.regex_match("a"));
+        CHECK(!Regex{"\\-"}.regex_match("a"));
+        CHECK(!Regex{"\\^"}.regex_match("a"));
+        CHECK(!Regex{"\\n"}.regex_match("a"));
+        CHECK(!Regex{"\\r"}.regex_match("a"));
+        CHECK(!Regex{"\\t"}.regex_match("a"));
+
+        CHECK(Regex{"(abc)"}.regex_match("abc"));
+        CHECK(!Regex{"(abc)"}.regex_match("adc"));
+
+
+        CHECK(Regex{"[abc]"}.regex_match("a"));
+        CHECK(Regex{"[abc]"}.regex_match("b"));
+        CHECK(Regex{"[abc]"}.regex_match("c"));
+        CHECK(!Regex{"[abc]"}.regex_match("d"));
+        CHECK(!Regex{"[^abc]"}.regex_match("a"));
+        CHECK(Regex{"[^abc]"}.regex_match("d"));
+        CHECK(Regex{"[a-c]"}.regex_match("a"));
+        CHECK(Regex{"[a-c]"}.regex_match("b"));
+        CHECK(Regex{"[a-c]"}.regex_match("c"));
+        CHECK(!Regex{"[a-c]"}.regex_match("d"));
+        CHECK(!Regex{"[^a-c]"}.regex_match("a"));
+        CHECK(Regex{"[^a-c]"}.regex_match("d"));
+        // CHECK(!Regex{"[^abc-e]"}.regex_match("e"));
+        // CHECK(Regex{"[^abc-e]"}.regex_match("d"));
+
+
+        CHECK(Regex{"\\p{Lu}"}.regex_match("A"));
+        CHECK(Regex{"\\p{Lu}"}.regex_match("Ä"));
+        CHECK(!Regex{"\\p{Lu}"}.regex_match("a"));
+        CHECK(!Regex{"\\p{Lu}"}.regex_match("ä"));
+        CHECK(!Regex{"\\p{Lu}"}.regex_match("5"));
+
+        // CHECK(Regex{"\\p{IsBasicLatin}"}.regex_match("5"));
+        // CHECK(!Regex{"\\p{IsBasicLatin}"}.regex_match("ü"));
+
+        CHECK(Regex{"."}.regex_match("5"));
+        CHECK(!Regex{"."}.regex_match("\n"));
+        CHECK(Regex{"\\s"}.regex_match(" "));
+        CHECK(!Regex{"\\s"}.regex_match("a"));
+        CHECK(Regex{"\\S"}.regex_match("a"));
+        // CHECK(Regex{"\\c"}.regex_match("a"));
+        // CHECK(!Regex{"\\c"}.regex_match("<"));
+        // CHECK(Regex{"\\C"}.regex_match("<"));
+        CHECK(Regex{"\\d"}.regex_match("5"));
+        CHECK(!Regex{"\\d"}.regex_match("a"));
+        CHECK(Regex{"\\D"}.regex_match("a"));
+        CHECK(Regex{"\\w\\w"}.regex_match("a5"));
+        CHECK(!Regex{"\\w"}.regex_match("-"));
+        CHECK(Regex{"\\W"}.regex_match("-"));
+
+        CHECK(Regex{"a"}.regex_search("_a_"));
+        CHECK(!Regex{"^a"}.regex_search("_a"));
+        CHECK(!Regex{"a$"}.regex_search("a_"));
+
+
+        CHECK(Regex{"a??"}.regex_match("a"));
+        CHECK(Regex{"a??"}.regex_match(""));
+        CHECK(!Regex{"a??"}.regex_match("aa"));
+
+        CHECK(Regex{"a*?"}.regex_match("a"));
+        CHECK(Regex{"a*?"}.regex_match("aaaa"));
+        CHECK(Regex{"a*?"}.regex_match(""));
+        CHECK(!Regex{"a*?"}.regex_match("b"));
+
+        CHECK(!Regex{"a+?"}.regex_match(""));
+        CHECK(Regex{"a+?"}.regex_match("a"));
+        CHECK(Regex{"a+?"}.regex_match("aaaaa"));
+        CHECK(!Regex{"a+?"}.regex_match("b"));
+
+        CHECK(!Regex{"a{2,5}?"}.regex_match("a"));
+        CHECK(Regex{"a{2,5}?"}.regex_match("aa"));
+        CHECK(Regex{"a{2,5}?"}.regex_match("aaaaa"));
+        CHECK(!Regex{"a{2,5}?"}.regex_match("aaaaaa"));
+        CHECK(!Regex{"a{2,5}?"}.regex_match("bbb"));
+
+        CHECK(Regex{"a{2}?"}.regex_match("aa"));
+        CHECK(!Regex{"a{2}?"}.regex_match("aaa"));
+        CHECK(!Regex{"a{2}?"}.regex_match("aaaa"));
+        CHECK(!Regex{"a{2}ß"}.regex_match("b"));
+
+        CHECK(Regex{"a{2,}?"}.regex_match("aa"));
+        CHECK(Regex{"a{2,}?"}.regex_match("aaa"));
+        CHECK(!Regex{"a{2,}?"}.regex_match("a"));
+        CHECK(!Regex{"a{2,}?"}.regex_match("b"));
+
+        CHECK(Regex{"a{0,2}?"}.regex_match("aa"));
+        CHECK(!Regex{"a{0,2}?"}.regex_match("aaa"));
+        CHECK(Regex{"a{0,2}?"}.regex_match("a"));
+        CHECK(!Regex{"a{0,2}?"}.regex_match("b"));
+
+        CHECK(Regex{"a{0,0}?"}.regex_match(""));
+        CHECK(!Regex{"a{0,0}?"}.regex_match("a"));
+
+
+        CHECK(Regex{"(abc)"}.regex_match("abc"));
+        CHECK(Regex{"(?:abc)"}.regex_match("abc"));
+        // CHECK(Regex{"(.)bc\\1"}.regex_match("abca"));
+        // CHECK(Regex{"(.)bc\\1"}.regex_match("xbcx"));
+        // CHECK(!Regex{"(.)bc\\1"}.regex_match("abcx"));
+
+        CHECK(Regex{".", RegexFlag::DotAll}.regex_match("\n"));
+        CHECK(Regex{"^a$", RegexFlag::Multiline}.regex_search("a\nxyz"));
+        CHECK(Regex{"abc", RegexFlag::CaseInsensitive}.regex_match("ABC"));
+        CHECK(Regex{"a b c", RegexFlag::RemoveWhitespace}.regex_match("abc"));
+        CHECK(Regex{".+", RegexFlag::Literal}.regex_match(".+"));
+        CHECK(!Regex{".", RegexFlag::Literal}.regex_match("a"));
+
+        std::string d = "a_a_x";
+        Regex{"a"}.make_replacer("b").regex_replace(d);
+        CHECK(d == "b_b_x");
+        Regex{"_(\\w)"}.make_replacer("-$0$1").regex_replace(d);
+        CHECK(d == "b-_bb-_xx");
+    }
+
     TEST_CASE("replacement translation") {
         Regex const r{"[0-9]"};
 
