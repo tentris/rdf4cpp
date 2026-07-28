48342a40d4354962a5d3cc23d7f176f80b8440c6
SandroMiudo <sandro@tentris.io>
2026-07-28 17:04:43 +0200

    add

--- src/rdf4cpp/BlankNode.hpp
+++ src/rdf4cpp/BlankNode.hpp
@@ -57,7 +57,7 @@ struct BlankNode : Node {
     /**
      * Validates the given blank node identifier
      * @param identifier identifier to validate
-     * @throws ParsingError if the Fblank node identifier is not valid
+     * @throws ParsingError if the blank node identifier is not valid
      */
     static void validate(std::string_view identifier);
 
