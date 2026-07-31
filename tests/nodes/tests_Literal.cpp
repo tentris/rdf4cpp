3c9afb83d9fccabbec50171c5cc3fe4780458241
Sandro Miudo <sandro@tentris.io>
2026-07-31 12:38:50 +0200

    Testing built in functions - return null nodes if invoked from null nodes (#462)

--- tests/nodes/tests_Literal.cpp
+++ tests/nodes/tests_Literal.cpp
@@ -1,4 +1,5 @@
 #define DOCTEST_CONFIG_IMPLEMENT
+
 #include <doctest/doctest.h>
 
 #include <rdf4cpp.hpp>
@@ -477,6 +478,146 @@ TEST_CASE("Literal - casting") {
         auto const lit2 = lit1.cast<Integer>();
 
         CHECK_EQ(lit2.value<Integer>(), -7);
+    }
+}
+
+// verify that operations based/on null node always return the null node
+TEST_CASE("Literal - null nodes") {
+    // ensure null node returns null node for specific functionality
+    Literal const null_node{};
+    Literal const null_node_cmp{};
+
+    Literal const stub_literal = Literal::make_simple("hello");
+    Literal const stub_literal_tagged = Literal::make_lang_tagged("hello", "en");
+
+    SUBCASE("upper case") {
+        CHECK(null_node.uppercase().null());
+    }
+
+    SUBCASE("lower case") {
+        CHECK(null_node.lowercase().null());
+    }
+
+    SUBCASE("langTag") {
+        CHECK(null_node.as_language_tag().null());
+    }
+
+    SUBCASE("langTag matches") {
+        CHECK(null_node.as_language_tag_matches_range("*").null());
+        CHECK(null_node.as_language_tag_matches_range(Literal::make_simple("en")).null());
+        CHECK(stub_literal_tagged.as_language_tag_matches_range(null_node).null());
+    }
+
+    SUBCASE("datatype") {
+        CHECK(null_node.datatype().null());
+    }
+
+    SUBCASE("abs") {
+        CHECK(null_node.abs().null());
+    }
+
+    SUBCASE("ceil") {
+        CHECK(null_node.ceil().null());
+    }
+
+    SUBCASE("floor") {
+        CHECK(null_node.floor().null());
+    }
+
+    SUBCASE("round") {
+        CHECK(null_node.round().null());
+    }
+
+    SUBCASE("concat") {
+        CHECK(null_node.concat(null_node_cmp).null());
+        CHECK(null_node.concat(stub_literal).null());
+        CHECK(stub_literal.concat(null_node).null());
+    }
+
+    SUBCASE("strlen") {
+        CHECK(null_node.as_strlen().null());
+    }
+
+    SUBCASE("contains") {
+        CHECK(null_node.as_contains("").null());
+    }
+
+    SUBCASE("str starts with") {
+        CHECK(null_node.as_str_starts_with("").null());
+    }
+
+    SUBCASE("str ends with") {
+        CHECK(null_node.as_str_ends_with("").null());
+    }
+
+    SUBCASE("substring before") {
+        CHECK(null_node.substr_before("").null());
+    }
+
+    SUBCASE("substring after") {
+        CHECK(null_node.substr_after("").null());
+    }
+
+    SUBCASE("year") {
+        CHECK(null_node.as_year().null());
+    }
+
+    SUBCASE("month") {
+        CHECK(null_node.as_month().null());
+    }
+
+    SUBCASE("day") {
+        CHECK(null_node.as_day().null());
+    }
+
+    SUBCASE("hours") {
+        CHECK(null_node.as_hours().null());
+    }
+
+    SUBCASE("minutes") {
+        CHECK(null_node.as_minutes().null());
+    }
+
+    SUBCASE("seconds") {
+        CHECK(null_node.as_seconds().null());
+    }
+
+    SUBCASE("timezone") {
+        CHECK(null_node.as_timezone().null());
+    }
+
+    SUBCASE("tz") {
+        CHECK(null_node.as_tz().null());
+    }
+
+    SUBCASE("md5") {
+        CHECK(null_node.md5().null());
+    }
+
+    SUBCASE("sha1") {
+        CHECK(null_node.sha1().null());
+    }
+
+    SUBCASE("sha256") {
+        CHECK(null_node.sha256().null());
+    }
+
+    SUBCASE("sha384") {
+        CHECK(null_node.sha384().null());
+    }
+
+    SUBCASE("sha512") {
+        CHECK(null_node.sha512().null());
+    }
+
+    SUBCASE("encode for uri") {
+        CHECK(null_node.encode_for_uri().null());
+    }
+
+    SUBCASE("is_* checks") {
+        CHECK_FALSE(null_node.is_numeric());
+        CHECK_FALSE(null_node.is_duration());
+        CHECK_FALSE(null_node.is_timepoint());
     }
 }
 
