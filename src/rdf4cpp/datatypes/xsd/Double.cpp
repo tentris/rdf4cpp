6fedae850e12556b93b0a7117c50b79bb5d0f0bb
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2026-09-03 11:04:37 +0200

    unecessary conversion

--- src/rdf4cpp/datatypes/xsd/Double.cpp
+++ src/rdf4cpp/datatypes/xsd/Double.cpp
@@ -77,7 +77,7 @@ std::optional<storage::identifier::LiteralID> capabilities::Inlineable<xsd_doubl
         return std::nullopt;
     }
 
-    return storage::identifier::LiteralID{util::pack<storage::identifier::LiteralID>(shortened)};
+    return util::pack<storage::identifier::LiteralID>(shortened);
 }
 
 template<>
